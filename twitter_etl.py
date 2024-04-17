45d969d52871ab507f415ab63bd844b7f25b6628
Pratiksha Vishvanath Ruthe <124226159+RuthePratiksha@users.noreply.github.com>
2024-04-17 16:17:37 +0530

    Update twitter_etl.py

--- twitter_etl.py
+++ twitter_etl.py
@@ -40,4 +40,4 @@ def run_twitter_etl():
         list.append(refined_tweet)
 
     df = pd.DataFrame(list)
-    df.to_csv('s3://sahas-airflow-one/refined_tweets.csv')
+    df.to_csv('s3://myprojectairflowbucketnew/refined_tweets.csv')
